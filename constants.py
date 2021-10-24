58065e7611918a532731a59274e406638d4ea799
gabrielzinCoelho <56321159+gabrielzinCoelho@users.noreply.github.com>
2021-10-24 12:51:46 -0300

    Create scene and render elements

---
+++ constants.py
@@ -0,0 +1,9 @@
+displayWidth = 960
+displayHeight = 540
+
+fps = 30
+
+defaultFontSize = 18
+defaultFontColor = (40, 40, 40)
+backgroundFilePath = './images/background.png'
+birdFilePath = './images/bird.png'
